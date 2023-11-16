105e8474b21a3192c69d0454ec4f40c86bc951b8
jimmy2jk <zhekatkach04@gmail.com>
2023-11-16 15:31:36 +0200

    Finished ReactFacts webpage

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import reactLogo from "../images/logo192.png";
+function Main() {
+    return (
+        <main className="Main">
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--list">
+                <li>Was first released in 2013</li>
+                <li>Was originaly created by Jordan Walke</li>
+                <li>Has well over 100K on GitHub</li>
+                <li>It's maintained by Facebook</li>
+                <li>
+                    Powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ul>
+        </main>
+    );
+}
+
+export default Main;
